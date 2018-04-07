a4bae73f201c4ad89a3928853a228e2d3cd3eabd
Katriel Cohn-Gordon <me@katriel.co.uk>
2018-04-07 19:25:12 +0100

    add a munger and some more data

--- munge.py
+++ munge.py
@@ -9,15 +9,40 @@ Date: 2017
 """
 
 import click
+import logging
+import matplotlib.pyplot as plt
+import pandas as pd
+
+log = logging.getLogger('fec')
 
 
 @click.command()
-def main():
+@click.argument('company-contributions', type=click.File(), default='data/facebook_contributions.csv')
+@click.argument('committees', type=click.File(), default='data/committees.csv')
+def main(company_contributions, committees):
     """
     Munge some FEC data files
     """
-    print('hello, world')
+    log.info(f'Reading {company_contributions.name} and {committees.name}...')
+    df = pd.read_table(committees, sep=',')
+    cid_to_party = df.set_index('committee_id').party
+
+    log.info('Munging')
+    contribs = pd.read_table(company_contributions, sep=',')
+    contribs = contribs[[
+        'committee_id',
+        'committee_name',
+        'contribution_receipt_amount',
+    ]]
+    contribs['party'] = contribs['committee_id'].map(cid_to_party)
+
+    log.info('Plotting')
+    contribs.groupby('party').sum().plot.pie(y='contribution_receipt_amount')
+    plt.show()
+
+    log.info('Bye!')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main()
