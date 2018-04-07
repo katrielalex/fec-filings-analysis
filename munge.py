7eb5079b87008b70913f3e6f8927716d5b100375
Katriel Cohn-Gordon <me@katriel.co.uk>
2018-04-07 17:30:54 +0100

    initial commit

---
+++ munge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Katriel's FEC data munger
+
+Author: Katriel Cohn-Gordon <me@katriel.co.uk>
+Date: 2017
+"""
+
+import click
+
+
+@click.command()
+def main():
+    """
+    Munge some FEC data files
+    """
+    print('hello, world')
+
+
+if __name__ == '__main__':
+    main()
